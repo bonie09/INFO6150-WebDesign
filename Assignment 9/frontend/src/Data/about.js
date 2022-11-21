00e80a9c5fc9b7d84b5ce6c8440c6aff891c4455
bonie09 <bonysachdev99@gmail.com>
2022-11-21 17:01:48 -0500

    New files added

---
+++ Assignment 9/frontend/src/Data/about.js
@@ -0,0 +1,30 @@
+const about = [
+  {
+    _id: "1",
+    name: "Utsav Chadha",
+    title: "Software Engineer II",
+    content:
+      "I made a few new friends and introduced myself to a lot of new teachers.",
+  },
+  {
+    _id: "2",
+    name: "Rohit Nawani",
+    title: "Software Engineer I",
+    content: "Learned how to create a server in node JS and my first API",
+  },
+  {
+    _id: "3",
+    name: "Puranjai Mendiratta",
+    title: "Co-Founder",
+    content: "Finished 2 seasons of Attack on Titan and My Hero academia.",
+  },
+  {
+    _id: 4,
+    name: "Utsav Chadha",
+    title: "Software Engineer I",
+    content:
+      "Made my first App in React JS, feels awesome to learn something new. I aim to be a full stack dev someday",
+  },
+];
+
+module.exports = about;
